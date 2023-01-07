d6aa60d870fda31ced03e199b3a47a7105bde5da
Cameron Fung <Cameronfuung@gmail.com>
2023-01-07 13:49:49 -0800

    added sample ui element to card

---
+++ ui/script.js
@@ -0,0 +1,20 @@
+// // Read the CSV file
+// fetch('tripFuelCons.csv')
+//   .then(response => response.text())
+//   .then((data) => {
+//     // Split the data into rows
+//     const rows = data.split('\n');
+
+//     console.table(rows)
+
+//     // Split each row into cells
+//     const cells = rows.map(row => row.split(','));
+
+//     // Access the cells in the 2nd row, 2nd column
+//     const cell = cells[1][1];
+
+//     // Do something with the cell data
+//     console.log(cell);
+//   });
+
+console.log("script loaded")
