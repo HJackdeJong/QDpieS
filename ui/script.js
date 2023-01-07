916714e386ca8ce3a6f3b0ca2cd12eaaf2bf0c0a
Cameron Fung <Cameronfuung@gmail.com>
2023-01-07 14:11:23 -0800

    added chart but data not showing

--- ui/script.js
+++ ui/script.js
@@ -1,20 +1,42 @@
-// // Read the CSV file
-// fetch('tripFuelCons.csv')
-//   .then(response => response.text())
-//   .then((data) => {
-//     // Split the data into rows
-//     const rows = data.split('\n');
+// Read the CSV file
+fetch('tripFuelCons.csv')
+  .then(response => response.text())
+  .then((data) => {
+    // Split the data into rows
+    const rows = data.split('\n');
+    console.table(rows)
 
-//     console.table(rows)
+    // Split each row into cells
+    const cells = rows.map(row => row.split(','));
 
-//     // Split each row into cells
-//     const cells = rows.map(row => row.split(','));
+    // Convert the cell data to floats
+    const floats = cells.map(cell => parseFloat(cell[1]));
 
-//     // Access the cells in the 2nd row, 2nd column
-//     const cell = cells[1][1];
+    // Do something with the float data
+    console.table(floats);
 
-//     // Do something with the cell data
-//     console.log(cell);
-//   });
 
+  // Create the chart
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        label: 'Mean',
+        data: floats
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom'
+        }]
+      }
+    }
+  });
+  });
+
+
+  
 console.log("script loaded")
