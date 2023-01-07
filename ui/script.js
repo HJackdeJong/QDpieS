605480a5079feba2b478497a90211dcfaffd66d2
Cameron Fung <Cameronfuung@gmail.com>
2023-01-07 15:27:04 -0800

    added card hover transition

--- ui/script.js
+++ ui/script.js
@@ -1,42 +1,78 @@
+
 // Read the CSV file
 fetch('tripFuelCons.csv')
   .then(response => response.text())
   .then((data) => {
     // Split the data into rows
     const rows = data.split('\n');
-    console.table(rows)
 
     // Split each row into cells
     const cells = rows.map(row => row.split(','));
-
-    // Convert the cell data to floats
-    const floats = cells.map(cell => parseFloat(cell[1]));
-
-    // Do something with the float data
-    console.table(floats);
-
-
-  // Create the chart
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'scatter',
-    data: {
-      datasets: [{
-        label: 'Mean',
-        data: floats
-      }]
-    },
-    options: {
-      scales: {
-        xAxes: [{
-          type: 'linear',
-          position: 'bottom'
-        }]
-      }
-    }
+    console.log(cells)
+    floats = cells.map(cell => {
+        if (parseFloat(cell[1]) > 500) {
+            cell[1] = 500
+        }
+        return [parseFloat(cell[0]), parseFloat(cell[1])]
+    })
+    console.log(floats)
+    a(floats);
   });
-  });
+//     // Do something with the float data
+
+
+//   // Create the chart
+//   const ctx = document.getElementById('myChart').getContext('2d');
+//   const chart = new Chart(ctx, {
+//     type: 'scatter',
+//     data: {
+//       datasets: [{
+//         label: 'Mean',
+//         data: floats
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         xAxes: [{
+//           type: 'linear',
+//           position: 'bottom',
+//           ticks: {
+//             max: 143,
+//             min:0
+//         }
+//         }],
+//         yAxes: [{
+//             ticks: {
+//                 max: 200,
+//                 min:0
+//             }
+//         }]
+//       }
+//     }
+//   });
+//   });
 
 
   
-console.log("script loaded")
+// console.log("script loaded")
+
+
+let a = (async function(data) {
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row[0]),
+        datasets: [
+          {
+            label: 'Average fuel usage per truck trip',
+            data: data.map(row => row[1]),
+            backgroundColor: "blue"
+          }
+        ]
+      }
+    }
+  );
+});
